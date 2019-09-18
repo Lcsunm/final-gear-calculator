a1fd879f1d5c8dacb8aa0efeadcb71c343e97ac4
Lcsunm <tri@lcsunm.cc>
2019-09-18 13:23:40 +0800

    -docs

---
+++ docs/precache-manifest.f1bcac45fc1d0a9529e86ac8ea84d70a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "c33a67254e6dc1181958",
+    "url": "/js/about.4c2bdc0a.js"
+  },
+  {
+    "revision": "dfe51861a00ec95bb4bc",
+    "url": "/css/app.6d4ebe73.css"
+  },
+  {
+    "revision": "dfe51861a00ec95bb4bc",
+    "url": "/js/app.21a8cb1d.js"
+  },
+  {
+    "revision": "1b2be7a69806ee204986",
+    "url": "/css/chunk-24868512.44f76ac6.css"
+  },
+  {
+    "revision": "1b2be7a69806ee204986",
+    "url": "/js/chunk-24868512.7939c658.js"
+  },
+  {
+    "revision": "f979e19ee725d91cbe4d",
+    "url": "/js/chunk-vendors.eb3fa035.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "d6afa53b54057a335b1062dc461e0070",
+    "url": "/index.html"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "43e39d335ce9b189a725e2d1d3b76759",
+    "url": "/weapon.json"
+  }
+];
